eae72d8c38f91f93ea797853b627d419e67c3aca
TheRustyKnife <therustyknife13@gmail.com>
2020-03-21 21:16:01 +0100

    Add error checking to icons

--- rusty-locale/icons.lua
+++ rusty-locale/icons.lua
@@ -5,13 +5,18 @@ local recipes = require '__rusty-locale__.recipes'
 local _M = {}
 
 
-function _M.of_generic(prototype)
+function _M.of_generic(prototype, silent)
 --- Get icons for the given prototype, assuming it's in the generic format.
 	if prototype.icons then
 		local icons = {}
 		for i, icon in pairs(prototype.icons) do
 			if icon.icon_size then icons[i] = icon
 			else
+				if not prototype.icon_size then
+					if silent then return nil; end
+					error(("%s/%s doesn't specify icon_size correctly"):format(prototype.type, prototype.name))
+				end
+				
 				local new = {}
 				for k, v in pairs(icon) do new[k] = v; end
 				new.icon_size = prototype.icon_size
@@ -24,24 +29,24 @@ function _M.of_generic(prototype)
 	return nil;
 end
 
-function _M.of_recipe(prototype)
+function _M.of_recipe(prototype, silent)
 --- Get icons for the given recipe prototype.
-	local icons = _M.of_generic(prototype)
+	local icons = _M.of_generic(prototype, silent)
 	if icons then return icons; end
 	
 	local product
 	if prototype.normal ~= nil then product = recipes.partial.get_main_product(prototype.normal)
 	else product = recipes.partial.get_main_product(prototype); end
 	
-	return product and _M.of(prototypes.find(product.name, product.type))
+	return product and _M.of(product.name, product.type, silent)
 end
 
 
-function _M.of(prototype, type)
+function _M.of(prototype, type, silent)
 --- Get the icons of the given prototype.
 	if type ~= nil then prototype = prototypes.find(prototype, type); end
-	if prototypes.inherits(prototype.type, 'recipe') then return _M.of_recipe(prototype)
-	else return _M.of_generic(prototype); end
+	if prototypes.inherits(prototype.type, 'recipe') then return _M.of_recipe(prototype, silent)
+	else return _M.of_generic(prototype, silent); end
 end
 
 
